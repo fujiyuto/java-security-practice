edbb93a5509329740f579642af01c9f298310f8a
fujiyuto <yuto.fujiwara.itfarm@gmail.com>
2026-01-24 18:00:21 +0900

    UserCreateRequest修正

--- src/main/java/com/example/auth_api/api/dto/user/request/UserCreateRequest.java
+++ src/main/java/com/example/auth_api/api/dto/user/request/UserCreateRequest.java
@@ -7,11 +7,13 @@ package com.example.auth_api.api.dto.user.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class UserCreateRequest {
 
     /**
